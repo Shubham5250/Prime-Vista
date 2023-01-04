c995458d8060686633a0e670d5296aa40290cbbc
Shubham5250 <shubhampaw312@gmail.com>
2023-01-04 23:27:44 +0530

    userEmailAndPassword-Auth-added

--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -1,22 +1,99 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
+    Button loginBtn;
+    EditText loginEmail,otpLogin;
+    ProgressDialog progressDialog;
+    String  emailpattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loginEmail = findViewById(R.id.loginEmail);
+        otpLogin = findViewById(R.id.otpLogin);
+        loginBtn = findViewById(R.id.loginBtn);
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                perfLogin();
+            }
+        });
 
     }
-    public void createAccount(View view){
-        Intent intent = new Intent(this, create_account.class);
+
+    private void perfLogin() {
+        String email = loginEmail.getText().toString();
+        String password = otpLogin.getText().toString();
+
+        if(!email.matches(emailpattern)){
+            loginEmail.setError("enter the correct email");
+        }
+        else if(password.isEmpty() || password.length()<8){
+            otpLogin.setError("Please enter the password with atleast 8 characters");
+        }
+
+        else {
+            progressDialog.setMessage("Please wait until login!");
+            progressDialog.setTitle("Login...");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(login.this, "Login successful..", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        progressDialog.dismiss();
+                        Toast.makeText(login.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+    }
+
+    private void sendUserToNextActivity() {
+
+        Intent intent = new Intent(this,goa_room_booking.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+
+
+    public void registration(View view){
+        Intent intent = new Intent(this,registration.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/example/myapplication/registration.java
@@ -0,0 +1,105 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class registration extends AppCompatActivity {
+
+    EditText inputEmail,inputPassword,confirmPassword;
+    Button register;
+    String  emailpattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        inputEmail = findViewById(R.id.loginEmail);
+        inputPassword = findViewById(R.id.otpLogin);
+        confirmPassword = findViewById(R.id.confirmPassword);
+        register = findViewById(R.id.register);
+        progressDialog = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PerfAuth();
+            }
+        });
+
+    }
+
+    private void PerfAuth() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        String confpassword = confirmPassword.getText().toString();
+
+        if(!email.matches(emailpattern)){
+            inputEmail.setError("enter the correct email");
+        }
+        else if(password.isEmpty() || password.length()<8){
+            inputPassword.setError("Please enter the password with atleast 8 characters");
+        }
+        else if(!password.equals(confpassword)){
+            confirmPassword.setError("Password didn't match");
+        }
+        else{
+            progressDialog.setMessage("Please while we register you!");
+            progressDialog.setTitle("Registration...");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(registration.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        progressDialog.dismiss();
+                        Toast.makeText(registration.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(registration.this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+
+    public void login(View view){
+        Intent intent = new Intent(this,login.class);
+        startActivity(intent);
+
+    }
+
+
+
+
+}
